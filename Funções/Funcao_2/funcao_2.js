ab1624cdff3c22c6d284f24ab3715a977817147c
gabrielestefono <gabrielestefono@hotmail.com>
2023-08-21 00:15:37 -0300

    Básico Sobre Funções #02

---
+++ Funções/Funcao_2/funcao_2.js
@@ -0,0 +1,36 @@
+function bomDia(){
+    return "Bom Dia!";
+}
+
+const BoaTarde = function (){
+    return "Boa Tarde!";
+}
+
+
+const BoaNoite = () => "Boa Noite!";
+
+// 1) Passar uma função como parâmetro para outra função
+function executarQualquerCoisa(fn){
+    if(typeof fn === 'function'){
+        console.log(fn())
+    }
+}
+
+executarQualquerCoisa(bomDia)
+executarQualquerCoisa(BoaTarde)
+executarQualquerCoisa(BoaNoite)
+executarQualquerCoisa(3)
+
+// 2) Retornar uma função a partir de uma outra função
+
+function potencia(base){
+    return function(exp){
+        return Math.pow(base, exp)
+    }
+}
+
+const potenciade2 = potencia(2)
+console.log(potenciade2(8))
+
+const potenciade3 = potencia(3)
+console.log(potenciade3(4))
