7efa77b4a456366d16c50604f27cb28305bec26d
gabrielestefono <gabrielestefono@hotmail.com>
2023-08-26 15:39:00 -0300

    Função Reduce #02

---
+++ Funções/Funcao_reduce_2/reduce.js
@@ -0,0 +1,24 @@
+const carrinho = [
+	{ nome: "Caneta", qtde: 10, preco: 7.99, fragil: true },
+	{ nome: "Impressora", qtde: 1, preco: 649.50, fragil: true },
+	{ nome: "Caderno", qtde: 4, preco:  27.10, fragil: false},
+	{ nome: "Lápis", qtde: 3, preco:  5.82, fragil: false},
+	{ nome: "Tesoura", qtde: 1, preco: 19.20, fragil: true },
+]
+
+Array.prototype.meuReduce = function (fn, inicial) {
+	let acc = inicial;
+	for (let i = 0; i < this.length; i++){
+		if (!acc && i === 0) {
+			acc = this[i]
+		} else {
+			acc = fn(acc, this[i], i, this)
+		}
+	}
+	return acc
+}
+
+const soma = (a, b) => a + b
+const mapear = a => a.qtde
+
+console.log(carrinho.map(mapear).meuReduce(soma))
