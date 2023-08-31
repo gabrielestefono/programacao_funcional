143766485c538b592792ed939c7abef96ee92a92
gabrielestefono <gabrielestefono@hotmail.com>
2023-08-30 22:46:09 -0300

    Promise #03 (   errei a numeração :'(    )

---
+++ Promise/Promise03/index.js
@@ -0,0 +1,12 @@
+function gerarNumerosEntre(min, max) {
+	if (min > max) {
+		[max, min] = [min, max]
+	}
+
+	return new Promise(resolve => {
+		const aleatorio = parseInt(Math.random() * (max - min + 1)) + min
+		resolve(aleatorio)
+	})
+}
+
+gerarNumerosEntre(10, 30).then(num => num * 10).then(num => `O numero gerado foi o numero ${num}.`).then(console.log);
