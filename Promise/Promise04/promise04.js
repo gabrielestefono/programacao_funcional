ad40e10f159b85fdbfe81456a97c718678ba4772
gabrielestefono <gabrielestefono@hotmail.com>
2023-10-01 22:33:56 -0300

    Promise 04 - 02

--- Promise/Promise04/promise04.js
+++ Promise/Promise04/promise04.js
@@ -10,3 +10,20 @@ function gerarNumerosEntre(min, max, tempo) {
 	})
 }
 
+function gerarVariosNumeros(){
+	return Promise.all([
+		gerarNumerosEntre(1, 60, 110),
+		gerarNumerosEntre(1, 60, 1000),
+		gerarNumerosEntre(1, 60, 90),
+		gerarNumerosEntre(1, 60, 800),
+		gerarNumerosEntre(1, 60, 70),
+		gerarNumerosEntre(1, 60, 600),
+		gerarNumerosEntre(1, 60, 50),
+		gerarNumerosEntre(1, 60, 400),
+		gerarNumerosEntre(1, 60, 30),
+		gerarNumerosEntre(1, 60, 200),
+		gerarNumerosEntre(1, 60, 10),
+	])
+}
+
+gerarVariosNumeros().then(numeros => console.log(numeros))
